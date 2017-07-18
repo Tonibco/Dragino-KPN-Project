9131aa8254a6da8e72c083bcbdbedf599548f5ee
Tonibco <toni@brouwertechnology.com>
2017-07-18 10:08:11 +0200

    Update Dragino_KPN.ino

--- Dragino_KPN.ino
+++ Dragino_KPN.ino
@@ -1,3 +1,4 @@
+
 /*******************************************************************************
  * Copyright (c) 2015 Thomas Telkamp and Matthijs Kooijman
  *
@@ -6,61 +7,46 @@
  * to do whatever they want with them without any restriction,
  * including, but not limited to, copying, modification and redistribution.
  * NO WARRANTY OF ANY KIND IS PROVIDED.
- *H
- * This example sends a valid LoRaWAN packet with payload "Hello,
- * world!", using frequency and encryption settings matching those of
- * the (early prototype version of) The Things Network.
+ *
+ * This example sends a valid LoRaWAN packet with payload 
+ * using frequency and encryption settings matching those of
+ * the The Things Network.
  *
  * Note: LoRaWAN per sub-band duty-cycle limitation is enforced (1% in g1,
  *  0.1% in g2).
  *
- * Change DEVADDR to a unique address!
- * See http://thethingsnetwork.org/wiki/AddressSpace
+ * 
+ * 
  *
  * Do not forget to define the radio type correctly in config.h.
  *
- *
- * Required Library: 
- *    * https://github.com/matthijskooijman/arduino-lmic 
- * 
- * Require Hardware:
- *    * LoRa Shield + Arduino
- *    * LoRa GPS Shield + Arduino 
- *    * LoRa Mini etc. 
  *******************************************************************************/
 
 #include <lmic.h>
 #include <hal/hal.h>
 #include <SPI.h>
-#include <RH_RF95.h>
-#include <String.h>
+#include <DHT.h>
+#include "ThingSpeak.h"
 
-RH_RF95 rf95;
 
-#define dht_dpin A0 // Use A0 pin as Data pin for DHT11. 
-byte bGlobalErr;
-char dht_dat[5]; // Store Sensor Data
-String stringOne;
-float frequency = 868.0;
+#define DHTPIN 3
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321  
+
+DHT dht(DHTPIN, DHTTYPE);
 
 // LoRaWAN NwkSKey, network session key
 // This is the default Semtech key, which is used by the prototype TTN
 // network initially.
-//ttn
-static const PROGMEM u1_t NWKSKEY[16] = { 0xf1, 0x73, 0xb9, 0xd3, 0xee, 0xc3, 0xf6, 0x47, 0x02,0xef, 0xc5, 0x90, 0x0b, 0x5a, 0x26, 0xd3 };
+static const PROGMEM u1_t NWKSKEY[16] = { 0xf1, 0x73, 0xb9, 0xd3, 0xee, 0xc3, 0xf6, 0x47, 0x02,0xef, 0xc5, 0x90, 0x0b, 0x5a, 0x26, 0xd3  };
+
 // LoRaWAN AppSKey, application session key
 // This is the default Semtech key, which is used by the prototype TTN
 // network initially.
-//ttn
 static const u1_t PROGMEM APPSKEY[16] = { 0xf3, 0x09, 0x04, 0x16, 0x97, 0x30, 0x1e, 0x87, 0x22, 0x8e, 0x7c, 0xba, 0xb2, 0x0c, 0x36, 0xc8 };
-// 
-//
+
 // LoRaWAN end-device address (DevAddr)
 // See http://thethingsnetwork.org/wiki/AddressSpace
-// ttn
-static const u4_t DEVADDR = 0x142061CF;
-
-
+static const u4_t DEVADDR = 0x142061CF; // <-- Change this address for every node!
 // These callbacks are only used in over-the-air activation, so they are
 // left empty here (we cannot leave them out completely unless
 // DISABLE_JOIN is set in config.h, otherwise the linker will complain).
@@ -68,12 +54,18 @@ void os_getArtEui (u1_t* buf) { }
 void os_getDevEui (u1_t* buf) { }
 void os_getDevKey (u1_t* buf) { }
 
-//static uint8_t mydata[] = "Hello world!";
-static osjob_t initjob,sendjob,blinkjob;
+unsigned long myChannelNumber = 266512;
+const char * myWriteAPIKey = "9ZGE0M7P684PRL6G";
+float data;
+String datastring="";
+char databuf[10];
+uint8_t dataoutgoing[10];
+
+static osjob_t sendjob;
 
 // Schedule TX every this many seconds (might become longer due to duty
 // cycle limitations).
-const unsigned TX_INTERVAL = 20;
+const unsigned TX_INTERVAL = 60;
 
 // Pin mapping
 const lmic_pinmap lmic_pins = {
@@ -82,56 +74,44 @@ const lmic_pinmap lmic_pins = {
     .rst = 9,
     .dio = {2, 6, 7},
 };
-void do_send(osjob_t* j){
-    // Check if there is not a current TX/RX job running
-    if (LMIC.opmode & OP_TXRXPEND) {
-        Serial.println("OP_TXRXPEND, not sending");
-    } else {
-        // Prepare upstream data transmission at the next possible time.
-        LMIC_setTxData2(1, dht_dat, sizeof(dht_dat)-1, 0);
-        Serial.println("Packet queued");
-        Serial.println(LMIC.freq);
-    }
-    // Next TX is scheduled after TX_COMPLETE event.
-}
 
 void onEvent (ev_t ev) {
     Serial.print(os_getTime());
     Serial.print(": ");
-    Serial.println(ev);
     switch(ev) {
         case EV_SCAN_TIMEOUT:
-            Serial.println("EV_SCAN_TIMEOUT");
+            Serial.println(F("EV_SCAN_TIMEOUT"));
             break;
         case EV_BEACON_FOUND:
-            Serial.println("EV_BEACON_FOUND");
+            Serial.println(F("EV_BEACON_FOUND"));
             break;
         case EV_BEACON_MISSED:
-            Serial.println("EV_BEACON_MISSED");
+            Serial.println(F("EV_BEACON_MISSED"));
             break;
         case EV_BEACON_TRACKED:
-            Serial.println("EV_BEACON_TRACKED");
+            Serial.println(F("EV_BEACON_TRACKED"));
             break;
         case EV_JOINING:
-            Serial.println("EV_JOINING");
+            Serial.println(F("EV_JOINING"));
             break;
         case EV_JOINED:
-            Serial.println("EV_JOINED");
+            Serial.println(F("EV_JOINED"));
             break;
         case EV_RFU1:
-            Serial.println("EV_RFU1");
+            Serial.println(F("EV_RFU1"));
             break;
         case EV_JOIN_FAILED:
-            Serial.println("EV_JOIN_FAILED");
+            Serial.println(F("EV_JOIN_FAILED"));
             break;
         case EV_REJOIN_FAILED:
-            Serial.println("EV_REJOIN_FAILED");
+            Serial.println(F("EV_REJOIN_FAILED"));
+            break;
             break;
         case EV_TXCOMPLETE:
-            Serial.println("EV_TXCOMPLETE (includes waiting for RX windows)");
+            Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
             if(LMIC.dataLen) {
                 // data received in rx slot after tx
-                Serial.print("Data Received: ");
+                Serial.print(F("Data Received: "));
                 Serial.write(LMIC.frame+LMIC.dataBeg, LMIC.dataLen);
                 Serial.println();
             }
@@ -139,31 +119,59 @@ void onEvent (ev_t ev) {
             os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
             break;
         case EV_LOST_TSYNC:
-            Serial.println("EV_LOST_TSYNC");
+            Serial.println(F("EV_LOST_TSYNC"));
             break;
         case EV_RESET:
-            Serial.println("EV_RESET");
+            Serial.println(F("EV_RESET"));
             break;
         case EV_RXCOMPLETE:
             // data received in ping slot
-            Serial.println("EV_RXCOMPLETE");
+            Serial.println(F("EV_RXCOMPLETE"));
             break;
         case EV_LINK_DEAD:
-            Serial.println("EV_LINK_DEAD");
+            Serial.println(F("EV_LINK_DEAD"));
             break;
         case EV_LINK_ALIVE:
-            Serial.println("EV_LINK_ALIVE");
+            Serial.println(F("EV_LINK_ALIVE"));
             break;
          default:
-            Serial.println("Unknown event");
+            Serial.println(F("Unknown event"));
             break;
     }
 }
 
+void do_send(osjob_t* j){
+
+  float a=(dht.readTemperature()*100); 
+  //float a=(dht.readTemperature()*100)*1.8 + 32; 
+
+                  // ThingSpeak.setField(2,h); 
+                  ThingSpeak.setField(1,a);
+                  ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);   // Send Data to IoT Server.
+
+int b=(int)a;
+  data = b;
+  datastring +=dtostrf(data, 4, 2, databuf);
+  strcpy((char *)dataoutgoing,databuf);
+  Serial.println(databuf);
+  
+    // Check if there is not a current TX/RX job running
+    if (LMIC.opmode & OP_TXRXPEND) {
+        Serial.println(F("OP_TXRXPEND, not sending"));
+    } else {
+         // Prepare upstream data transmission at the next possible time.
+        LMIC_setTxData2(1, dataoutgoing, sizeof(dataoutgoing), 0);
+        Serial.println(F("Packet queued"));
+    }
+    // Next TX is scheduled after TX_COMPLETE event.
+}
+
 void setup() {
-   /* Serial.begin(9600);
-    while(!Serial);
-    Serial.println("Starting");
+    Serial.begin(115200);
+    Serial.println(F("Starting"));
+    ThingSpeak.begin();
+    
+
     #ifdef VCC_ENABLE
     // For Pinoccio Scout boards
     pinMode(VCC_ENABLE, OUTPUT);
@@ -175,7 +183,7 @@ void setup() {
     os_init();
     // Reset the MAC state. Session and pending data transfers will be discarded.
     LMIC_reset();
-    //LMIC_setClockError(MAX_CLOCK_ERROR * 1/100);
+
     // Set static session parameters. Instead of dynamically establishing a session
     // by joining the network, precomputed session parameters are be provided.
     #ifdef PROGMEM
@@ -191,210 +199,39 @@ void setup() {
     // If not running an AVR with PROGMEM, just use the arrays directly 
     LMIC_setSession (0x1, DEVADDR, NWKSKEY, APPSKEY);
     #endif
-    
+
+    // Set up the channels used by the Things Network, which corresponds
+    // to the defaults of most gateways. Without this, only three base
+    // channels from the LoRaWAN specification are used, which certainly
+    // works, so it is good for debugging, but can overload those
+    // frequencies, so be sure to configure the full frequency range of
+    // your network here (unless your network autoconfigures them).
+    // Setting up channels should happen after LMIC_setSession, as that
+    // configures the minimal channel set.
+    LMIC_setupChannel(0, 868100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(1, 868300000, DR_RANGE_MAP(DR_SF12, DR_SF7B), BAND_CENTI);      // g-band
+    LMIC_setupChannel(2, 868500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(3, 867100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(4, 867300000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(5, 867500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(6, 867700000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(7, 867900000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(8, 868800000, DR_RANGE_MAP(DR_FSK,  DR_FSK),  BAND_MILLI);      // g2-band
+    // TTN defines an additional channel at 869.525Mhz using SF9 for class B
+    // devices' ping slots. LMIC does not have an easy way to define set this
+    // frequency and support for class B is spotty and untested, so this
+    // frequency is not configured here.
+
     // Disable link check validation
     LMIC_setLinkCheckMode(0);
 
-    // TTN uses SF9 for its RX2 window.
-    LMIC.dn2Dr = DR_SF9;
-    
     // Set data rate and transmit power (note: txpow seems to be ignored by the library)
     LMIC_setDrTxpow(DR_SF7,14);
 
     // Start job
-    do_send(&sendjob);*/
-
-    InitDHT();
-    Serial.begin(9600);
-    if (!rf95.init())
-        Serial.println("init failed");
-    // Setup ISM frequency
-    rf95.setFrequency(frequency);
-    // Setup Power,dBm
-    rf95.setTxPower(13);
-    
-    Serial.println("Humidity and temperature\n\n"); 
+    do_send(&sendjob);
 }
-
-void InitDHT()
-{
-    pinMode(dht_dpin,OUTPUT);//Set A0 to output
-    digitalWrite(dht_dpin,HIGH);//Pull high A0
-}
-
-//Get Sensor Data
-void ReadDHT()
-{
-    bGlobalErr=0;
-    byte dht_in;
-    byte i;
-        
-    //pinMode(dht_dpin,OUTPUT);
-    digitalWrite(dht_dpin,LOW);//Pull Low A0 and send signal
-    delay(30);//Delay > 18ms so DHT11 can get the start signal
-        
-    digitalWrite(dht_dpin,HIGH);
-    delayMicroseconds(40);//Check the high level time to see if the data is 0 or 1
-    pinMode(dht_dpin,INPUT);
-    // delayMicroseconds(40);
-    dht_in=digitalRead(dht_dpin);//Get A0 Status
-    //   Serial.println(dht_in,DEC);
-    if(dht_in){
-        bGlobalErr=1;
-        return;
-    }
-    delayMicroseconds(80);//DHT11 send response, pull low A0 80us
-    dht_in=digitalRead(dht_dpin);
-    
-    if(!dht_in){
-        bGlobalErr=2;
-        return;
-    }
-    delayMicroseconds(80);//DHT11 send response, pull low A0 80us
-    for (i=0; i<5; i++)//Get sensor data
-    dht_dat[i] = read_dht_dat();
-    pinMode(dht_dpin,OUTPUT);
-    digitalWrite(dht_dpin,HIGH);//release signal and wait for next signal
-    byte dht_check_sum = dht_dat[0]+dht_dat[1]+dht_dat[2]+dht_dat[3];//calculate check sum
-    if(dht_dat[4]!= dht_check_sum)//check sum mismatch
-        {bGlobalErr=3;}
-};
-
-byte read_dht_dat(){
-    byte i = 0;
-    byte result=0;
-    for(i=0; i< 8; i++)
-    {
-        while(digitalRead(dht_dpin)==LOW);//wait 50us
-        delayMicroseconds(30);//Check the high level time to see if the data is 0 or 1
-        if (digitalRead(dht_dpin)==HIGH)
-        result |=(1<<(7-i));//
-        while (digitalRead(dht_dpin)==HIGH);//Get High, Wait for next data sampleing. 
-    }
-    return result;
-}
-uint16_t calcByte(uint16_t crc, uint8_t b)
-{
-    uint32_t i;
-    crc = crc ^ (uint32_t)b << 8;
-    
-    for ( i = 0; i < 8; i++)
-    {
-        if ((crc & 0x8000) == 0x8000)
-            crc = crc << 1 ^ 0x1021;
-        else
-            crc = crc << 1;
-    }
-    return crc & 0xffff;
-}
-
-uint16_t CRC16(uint8_t *pBuffer,uint32_t length)
-{
-    uint16_t wCRC16=0;
-    uint32_t i;
-    if (( pBuffer==0 )||( length==0 ))
-    {
-      return 0;
-    }
-    for ( i = 0; i < length; i++)
-    { 
-      wCRC16 = calcByte(wCRC16, pBuffer[i]);
-    }
-    return wCRC16;
-}
-
 
 void loop() {
-    //os_runloop_once();
-ReadDHT();
-    char data[50] = {0} ;
-    // Use data[0], data[1],data[2] as Node ID
-    data[0] = 1 ;
-    data[1] = 1 ;
-    data[2] = 1 ;
-    data[3] = dht_dat[0];//Get Humidity
-    data[4] = dht_dat[2];//Get Temperature
-    switch (bGlobalErr)
-    {
-      case 0:
-          Serial.print("Current humdity = ");
-          Serial.print(data[3], DEC);//Show humidity
-          Serial.print(".");
-          Serial.print(dht_dat[1], DEC);//Show humidity
-          Serial.print("%  ");
-          Serial.print("temperature = ");
-          Serial.print(data[4], DEC);//Show temperature
-          Serial.print(".");
-          Serial.print(dht_dat[3], DEC);//Show temperature
-          Serial.println("C  ");
-          break;
-       case 1:
-          Serial.println("Error 1: DHT start condition 1 not met.");
-          break;
-       case 2:
-          Serial.println("Error 2: DHT start condition 2 not met.");
-          break;
-       case 3:
-          Serial.println("Error 3: DHT checksum error.");
-          break;
-       default:
-          Serial.println("Error: Unrecognized code encountered.");
-          break;
-    }
-    int dataLength = strlen(data);//CRC length for LoRa Data
-    //Serial.println(dataLength);
-     
-    uint16_t crcData = CRC16((unsigned char*)data,dataLength);//get CRC DATA
-    //Serial.println(crcData);
-       
-    unsigned char sendBuf[50]={0};
-    int i;
-    for(i = 0;i < 8;i++)
-    {
-        sendBuf[i] = data[i] ;
-    }
-    
-    sendBuf[dataLength] = (unsigned char)crcData; // Add CRC to LoRa Data
-    //Serial.println( sendBuf[dataLength] );
-    
-    sendBuf[dataLength+1] = (unsigned char)(crcData>>8); // Add CRC to LoRa Data
-    //Serial.println( sendBuf[dataLength+1] );
-
-    rf95.send(sendBuf, strlen((char*)sendBuf));//Send LoRa Data
-    //Serial.print(strlen((char*)sendBuf));
-     
-    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];//Reply data array
-    uint8_t len = sizeof(buf);//reply data length
-
-    if (rf95.waitAvailableTimeout(3000))// Check If there is reply in 3 seconds.
-    {
-        // Should be a reply message for us now   
-        if (rf95.recv(buf, &len))//check if reply message is correct
-       {
-            if(buf[0] == 1||buf[1] == 1||buf[2] ==1) // Check if reply message has the our node ID
-           {
-               pinMode(4, OUTPUT);
-               digitalWrite(4, HIGH);
-               Serial.print("got reply: ");//print reply
-               Serial.println((char*)buf);
-              
-               delay(400);
-               digitalWrite(4, LOW); 
-               //Serial.print("RSSI: ");  // print RSSI
-               //Serial.println(rf95.lastRssi(), DEC);        
-           }    
-        }
-        else
-        {
-           Serial.println("recv failed");//
-           rf95.send(sendBuf, strlen((char*)sendBuf));//resend if no reply
-        }
-    }
-    else
-    {
-        Serial.println("No reply, is rf95_server running?");//No signal reply
-        rf95.send(sendBuf, strlen((char*)sendBuf));//resend data
-    }
-    delay(30000); // Send sensor data every 30 seconds
-    
+    os_runloop_once();
 }
